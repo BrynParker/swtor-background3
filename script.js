525ad56ac142354c045fac67657568f28d603a98
Civic Networks <44575209+CivicNetworks@users.noreply.github.com>
2025-08-19 18:15:08 -0500

    Initial upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+
+no JS here . . . 
+
+  # made in 2016 by Nazar The Vis Azhar
+  # last code updated on 11/November/2022
+  # https://www.nazarazhar.com/
+
+I just wanted to make people happy :)
+
+**/
